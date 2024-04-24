65c59d7f652ecd7e11959226c52a81b058777198
panyongxu1002 <panyx1002@gmail.com>
2024-04-24 14:20:44 +0800

    add 渐变字体

--- packages/app/src/components/Card.tsx
+++ packages/app/src/components/Card.tsx
@@ -14,9 +14,12 @@ const HeroSection: React.FC<HeroSectionProps> = ({ title, description, ctaText, 
   return (
     <section className='flex justify-center h-[892px] items-center px-16 py-20 w-full text-white bg-zinc-900 max-md:px-5 max-md:max-w-full'>
       <div className='flex flex-col items-center mt-10 mb-4 max-w-full w-[791px]'>
-        <img loading='lazy' src={imageSrc} alt='Hero image' className='w-[240px] max-w-full aspect-square' />
+        <Image loading='lazy' src={imageSrc} alt='Hero image' className='w-[240px] max-w-full aspect-square' />
         <h1 className='self-stretch mt-16 text-7xl font-bold text-center bg-clip-text leading-[88px] text-ellipsis max-md:mt-10 max-md:max-w-full max-md:text-4xl max-md:leading-[49px]'>
           {title}
+          <span className='bg-gradient-to-l bg-clip-text  from-green-500 via-green-400 to-green-600 text-transparent'>
+            Airdrop
+          </span>
         </h1>
         <p className='self-stretch mt-5 text-3xl font-medium leading-9 text-center text-ellipsis text-stone-300 max-md:max-w-full'>
           {description}
@@ -34,7 +37,7 @@ export function Card() {
   return (
     <main>
       <HeroSection
-        title='Stage One Airdrop'
+        title='Stage One '
         description='Lorem ipsum dolor sit amet consectetur. Malesuada dictumst nunc egestas elementum. Eget augue urna laoreet facilisis.'
         ctaText='Connect Wallet'
         ctaSubtext='Connect to join the airdrop'
